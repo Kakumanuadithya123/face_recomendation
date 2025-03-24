c38350705f5102a4e96c01c33815b83226219add
akumanuadithya123 <21bq1a4258@vvit.net>
2025-03-24 22:34:18 +0530

    new

--- majorproject/settings.py
+++ majorproject/settings.py
@@ -160,9 +160,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "login/static",
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 STATIC_ROOT = BASE_DIR / "static_files"
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
